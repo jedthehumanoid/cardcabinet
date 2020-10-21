6739b8691bd25fb498dea0bc82d4b3e238cd189b
Mattias Jadelius <212982+jedthehumanoid@users.noreply.github.com>
2020-10-21 15:30:24 +0200

    Added some simple tests

--- card_test.go
+++ card_test.go
@@ -2,14 +2,48 @@ package cardcabinet
 
 import (
 	"encoding/json"
-	"fmt"
+	"io/ioutil"
+	"strings"
 	"testing"
+	//	"fmt"
 )
 
 func TestReadCard(t *testing.T) {
 	card, _ := ReadCard("testdata/card.md")
-	s, _ := json.MarshalIndent(card, "", "   ")
-	fmt.Println(string(s))
-	//	t.Errorf("Abs(-1) = %d; want 1", got)
 
+	result := toJSONIndent(card)
+	
+	b, _ := ioutil.ReadFile("testdata/card.json")
+	expected := strings.TrimSpace(string(b))
+
+	if result != expected {
+		t.Errorf("unexpected result, got:\n%s\n, expected:\n%s\n", result, expected)
+	}
 }
+
+func TestReadCards(t *testing.T) {
+	cards := ReadCards("testdata/")
+
+	c := []string{}
+	for _, card := range cards {
+		c = append(c, card.Name)
+	}
+	result := toJSON(c)
+	
+	expected := toJSON([]string{"add-magnets.md", "card.md"})
+
+	if result != expected {
+		t.Errorf("unexpected result, got: %s, expected: %s", result, expected)
+	}
+}
+
+func toJSON(i interface{}) string {
+	b, _ := json.Marshal(i)
+	return string(b)
+}
+
+func toJSONIndent(i interface{}) string {
+	b, _ := json.MarshalIndent(i, "", "   ")
+	return string(b)
+}
+
