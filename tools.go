49879623376bae7aa4134941c53459e57b6b7287
Mattias Jadelius <212982+jedthehumanoid@users.noreply.github.com>
2021-10-13 22:00:40 +0200

    Changes

--- tools.go
+++ tools.go
@@ -3,12 +3,7 @@ package cardcabinet
 import (
 	"os"
 	"path/filepath"
-	"regexp"
 	"strings"
-	"unicode"
-
-	"golang.org/x/text/transform"
-	"golang.org/x/text/unicode/norm"
 	"io/ioutil"
 	
 )
@@ -45,26 +40,6 @@ func GetPath(title string) string {
 		ret = ret + "/"
 	}
 	return ret
-}
-
-// FromSlug returns "this format" from "this-format"
-func FromSlug(s string) string {
-	return strings.Replace(s, "-", " ", -1)
-}
-
-// ToSlug returns a "page slug" out of string
-// Makes lowercase, removes international accents, turns spaces and
-// other nonalphanumeric characters to dashes, but keeps slashes
-func ToSlug(s string) string {
-	var re = regexp.MustCompile("[^a-z0-9/]+")
-
-	s = strings.ToLower(s)
-	t := transform.Chain(norm.NFD, transform.RemoveFunc(func(r rune) bool {
-		return unicode.Is(unicode.Mn, r)
-	}), norm.NFC)
-	s, _, _ = transform.String(t, s)
-	s = re.ReplaceAllString(s, "-")
-	return strings.Trim(s, "-")
 }
 
 func asStringSlice(i interface{}) []string {
