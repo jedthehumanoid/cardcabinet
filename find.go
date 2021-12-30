05222828b24bfef0d6d2ecbe8d80fc18337121ba
Mattias Jadelius <212982+jedthehumanoid@users.noreply.github.com>
2021-12-30 14:36:42 +0100

    Add find

---
+++ find.go
@@ -0,0 +1,34 @@
+package cardcabinet
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+// FindFiles is like find
+func findFiles(dir string, recursive bool) []string {
+	files := []string{}
+	if !recursive {
+		dirfiles, _ := ioutil.ReadDir(dir)
+		for _, file := range dirfiles {
+			// mfr filter os.fileinfo
+			if !file.IsDir() {
+				files = append(files, file.Name())
+			}
+		}
+	} else {
+		filepath.Walk(dir,
+			func(file string, f os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+				// mfr filter os.fileinfo
+				if !f.IsDir() {
+					files = append(files, file)
+				}
+				return nil
+			})
+	}
+	return files
+}
